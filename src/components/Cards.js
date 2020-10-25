5e62cfd06b5394d81be3411b1da5e7c49fd8d87c
Melanie <melaniechele@gmail.com>
2020-10-24 21:34:04 -0400

    Did Rick and Morty project

---
+++ src/components/Cards.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const Square = styled.div`
+    border: .5rem solid black;
+    width: 40%;
+    font-size: .8rem;
+    margin: 2rem;
+`
+
+const Img = styled.img`
+    margin: 10px;
+    width:60%;
+    height:60%;
+
+`
+
+function Cards(props){
+
+console.log(props)
+
+
+
+
+    return (
+        
+        <Square>
+            <h1>{props.name}</h1>
+            <h4>Status: {props.status}</h4>
+            <h4>Species: {props.species}</h4>
+            <h5>Location: {props.location}</h5>
+            <Img alt= {`Rick and Morty pictures`} src={props.image}/>
+        </Square>
+        
+
+
+
+    )
+}
+
+export default Cards
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,35 @@
 // Write your Character component here
+import React from 'react'
+import Cards from './Cards'
+import styled from 'styled-components'
+
+const Flexible = styled.div`
+
+    display:flex;
+    flex-wrap: wrap;
+    justify-content:center;
+  
+    `
+function Character(props){
+    
+   const {data} = props;
+    
+console.log(props)
+
+    
+
+    return (
+
+        <Flexible>
+
+        {data.map((element, i)=>{
+             return ( <Cards key = {i} name={element.name} status={element.status} species = {element.species} image = {element.image} location={element.location.name} />)
+        })}
+        </Flexible>
+        
+        )
+
+}
+
+
+export default Character
